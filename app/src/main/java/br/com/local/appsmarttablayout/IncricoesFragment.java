f4e75a4624ab10b714661f03dcefa9ce02a79316
Laercio-pc <lndsilva@hotmail.com>
2020-07-22 15:36:56 -0300

    Initial commit

---
+++ app/src/main/java/br/com/local/appsmarttablayout/IncricoesFragment.java
@@ -0,0 +1,24 @@
+package br.com.local.appsmarttablayout;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class IncricoesFragment extends Fragment {
+
+    public IncricoesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_incricoes, container, false);
+    }
+}
---
+++ app/src/main/java/br/com/local/appsmarttablayout/MainActivity.java
@@ -0,0 +1,41 @@
+package br.com.local.appsmarttablayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
+public class MainActivity extends AppCompatActivity {
+    SmartTabLayout smartTabLayout;
+    ViewPager viewPager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setElevation(0);
+
+        smartTabLayout = findViewById(R.id.viewPagerTab);
+        viewPager = findViewById(R.id.viewPager);
+
+        FragmentPagerAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(),
+                FragmentPagerItems.with(this)
+                        .add("Home", HomeFragment.class)
+                        .add("Inscrições", IncricoesFragment.class)
+                        .add("Em alta", EmAltaFragment.class)
+                        .create()
+        );
+        viewPager.setAdapter(adapter);
+        smartTabLayout.setViewPager(viewPager);
+
+
+    }
+}
